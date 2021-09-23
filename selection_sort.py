b96c7a48830a924f013ad383bdc28a7645d968c1
Kaiser Konok <85292406+kaiserkonok@users.noreply.github.com>
2021-09-23 13:10:08 +0600

    added selection sort algorithm

--- selection_sort.py
+++ selection_sort.py
@@ -1,7 +1,7 @@
 def selection_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         min_index = i
-        for j in range(i + 1, len(arr) - 1):
+        for j in range(i + 1, len(arr)):
             if arr[min_index] > arr[j]:
                 min_index = j
 
